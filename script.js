5e0ebb9dc36ba0fcbc0797d0f6f72acefa5b4681
Marius Pislaru <m.r.pislaru@gmail.com>
2022-07-31 20:37:32 +0300

    storing favorites to session

--- script.js
+++ script.js
@@ -10,12 +10,8 @@ const btnFavorites = document.querySelector('#btn-favorites');
 const btnDiscover = document.querySelector('#btn-discover');
 const btnClearSearch = document.querySelector('#clear-search');
 
-// const genres = fetch(`${baseURL}genre/movie/list?${auth}`).then(
-//   (response) => response.json()
-// );
-
-const favorites = [];
-const toPush = new Set();
+const favorites = JSON.parse(sessionStorage.getItem('favoriteMovies')) ?? [];
+let currentTab;
 
 const initMovies = () => {
 	setActiveTab(btnDiscover);
@@ -23,9 +19,11 @@ const initMovies = () => {
 	fetch(`${baseURL}discover/movie?${auth}`)
 		.then((response) => response.json())
 		.then((data) => {
-			// console.log(data);
 			populateMovies(data.results);
-		});
+		})
+		.catch((err) =>
+			console.log(`Error when initializing discover tab: ${err}`)
+		);
 };
 initMovies();
 
@@ -34,7 +32,9 @@ function populateMovies(movies) {
 	movies.forEach((movie) => {
 		// console.log(movie);
 		discoverContainer.innerHTML += `<div class="movie-box">
+    <div class="thumbnail-container">
     <img src="https://image.tmdb.org/t/p/w300/${movie.backdrop_path}" />
+    </div>
     <div class="movie-info">
     <p class="movie-info--title">${movie.original_title}</p>
     <p class="movie-info--release">${movie.release_date}</p>
@@ -58,7 +58,8 @@ function searchMovie() {
 			.then((data) => {
 				console.log(data);
 				populateMovies(data.results);
-			});
+			})
+			.catch((err) => console.log(`Error when searching: ${err}`));
 		btnClearSearch.classList.add('active');
 	} else {
 		initMovies();
@@ -66,59 +67,65 @@ function searchMovie() {
 }
 
 function setFavorite() {
-	if (this.checked) {
-		// console.log(this);
-		fetch(`${baseURL}movie/${this.getAttribute('data-id')}?${auth}`)
-			.then((response) => response.json())
-			.then((data) => {
-				// console.log(data);
-				favorites.push(data);
-				// const { id, backdrop_path, original_title, release_date } = data;
-				const { id, backdrop_path, original_title, release_date } = data;
-				toPush.add({
-					id,
-					backdrop_path,
-					original_title,
-					release_date,
-				});
-				console.group('current favorites', toPush);
-				favorites.forEach((favorite, index) =>
-					console.log(`${index}: ${favorite.original_title}`)
-				);
-				console.groupEnd();
-			});
-		this.classList.add('green-flair');
-		setTimeout(() => this.classList.remove('green-flair'), 500);
-	} else {
-		console.log('unselected item:', this.getAttribute('data-id'));
-		console.log(
-			'index found:',
-			favorites.findIndex((movie) => movie.id == this.getAttribute('data-id'))
-		);
-		favorites.splice(
-			favorites.findIndex((movie) => movie.id == this.getAttribute('data-id')),
-			1
-		);
-		console.group('current favorites');
-		favorites.forEach((favorite, index) =>
-			console.log(`${index}: ${favorite.original_title}`)
-		);
-		console.groupEnd();
-	}
+	const favoritedId = this.getAttribute('data-id');
+	const byeGrandpa = () => {
+		this.parentElement.parentElement.classList.add('fade-out');
+		setTimeout(() => {
+			this.parentElement.parentElement.remove();
+		}, 500);
+	};
 
-	this.classList.toggle('favorited');
+	const attemptSetFavorite = new Promise((resolve, reject) => {
+		if (this.checked) {
+			fetch(`${baseURL}movie/${favoritedId}?${auth}`)
+				.then((response) => response.json())
+				.then((data) => {
+					favorites.push(data);
+					resolve('favorited');
+				})
+				.catch((err) => reject(`set favorite error: ${err}`));
+			this.classList.add('green-flair');
+			setTimeout(() => this.classList.remove('green-flair'), 500);
+		} else {
+			try {
+				favorites.splice(
+					favorites.findIndex((movie) => movie.id == favoritedId),
+					1
+				);
+				currentTab === 'Favorites' && byeGrandpa();
+				resolve('unfavorited');
+			} catch (err) {
+				reject(`set unfavorite error: ${err}`);
+			}
+		}
+	});
+
+	attemptSetFavorite
+		.then((message) => {
+			console.log(favoritedId, message);
+			this.classList.toggle('favorited');
+			sessionStorage.setItem('favoriteMovies', JSON.stringify(favorites));
+		})
+		.catch((message) => {
+			console.log(message);
+		});
 }
 
 function setAlreadyFavorited(movieList) {
-	favorites.forEach((favorite) => {
-		if (movieList.find((movie) => movie.id === favorite.id)) {
-			const currentFavorite = document.querySelector(
-				`.favorite-checkmark[data-id="${favorite.id}"]`
-			);
-			currentFavorite.checked = true;
-			currentFavorite.classList.add('favorited');
-		}
-	});
+	try {
+		favorites &&
+			favorites.forEach((favorite) => {
+				if (movieList.find((movie) => movie.id === favorite.id)) {
+					const currentFavorite = document.querySelector(
+						`.favorite-checkmark[data-id="${favorite.id}"]`
+					);
+					currentFavorite.checked = true;
+					currentFavorite.classList.add('favorited');
+				}
+			});
+	} catch (err) {
+		console.log(`Error when setting already favorited: ${err}`);
+	}
 }
 
 let debounceTimer;
@@ -127,10 +134,10 @@ const debounce = (cb, time) => {
 	debounceTimer = window.setTimeout(cb, time);
 };
 searchField.addEventListener('input', () => debounce(searchMovie, 1000));
+
 btnDiscover.addEventListener('click', () => {
 	initMovies();
 });
-
 btnFavorites.addEventListener('click', () => {
 	setActiveTab(btnFavorites);
 	populateMovies(favorites);
@@ -151,4 +158,5 @@ btnClearSearch.addEventListener('click', () => {
 function setActiveTab(targetBtn) {
 	document.querySelector('.btn.active')?.classList.remove('active');
 	targetBtn.classList.toggle('active');
+	currentTab = targetBtn.textContent;
 }
